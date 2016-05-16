3a5ac0da6d4750679418461eacfdee585beff5cc
Andrew Russo <russspruce@gmail.com>
2016-05-16 16:35:44 -0700

    Added front end forms for language to the form as well as creating the first part of the App.java file to follow the file path of the form.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,6 +15,32 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-    
+
+    get("/languages/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/language-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/languages", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("languages", Stylist.all());
+      model.put("template", "templates/languages.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/languages", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String description = request.queryParams("description");
+      String example = request.queryParams("example");
+      String date = request.queryParams("date");
+      String url = request.queryParams("url");
+      Stylist newStylist = new Stylist(styler);
+      newStylist.save(); //*** ADDED FOR DB VERSION ***
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
