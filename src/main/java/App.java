a622ca96d8e2d779d2ba62e8899cdf88188dd453
Ian Lesikar <ilesikar@gmail.com>
2016-05-18 09:09:38 -0700

    added career.vtl, App.java integration

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,28 +9,37 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    get("/", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("careers", Career.all());
-      model.put("template", "templates/index.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/career-form", (request, response) -> {
+    get("/career/add", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("career", request.session().attribute("career"));
-      model.put("template", "templates/career-form.vtl");
+      model.put("template", "templates/career-add.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/", (request, response) -> {
+    post("/career/add", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
       String title = request.queryParams("title");
       String description = request.queryParams("description");
-      Career newCareer = new Career(title,description);
+      Career newCareer = new Career(title, description);
       newCareer.save();
-      response.redirect("/");
-      return null;
-    });
+      model.put("Template", "templates/career");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    get("/careers", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("careers", Career.all());
+      model.put("template", "templates/careers.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/career/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int careerId = Integer.parseInt(request.params(":id"));
+      Career career = Career.find(careerId);
+      model.put("career", career);
+      model.put("template", "templates/career.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
